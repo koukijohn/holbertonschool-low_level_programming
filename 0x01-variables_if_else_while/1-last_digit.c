127b515fdc9e97ab47b59b3f827e7cd0179dc703
KoukiJohn <koukijohn@hotmail.com>
2018-01-19 01:22:15 +0000

    added 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function will store a diff value every time you run this program
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	else if (n < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, x);
+	else
+		printf("Last digit of %d is % d and is 0\n", n, x);
+	return (0);
+}
