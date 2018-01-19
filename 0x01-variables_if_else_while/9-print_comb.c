ac9c7c154105fc2e4c8f90845a65fdc5e85d7148
KoukiJohn <koukijohn@hotmail.com>
2018-01-19 04:37:15 +0000

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+	for (; i <= '9'; i++)
+	{
+		putchar (i);
+		if (i < '9')
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
