0d48154d257d9ea78bf22407ea7049764e2282ee
KoukiJohn <koukijohn@hotmail.com>
2018-01-23 23:21:11 +0000

    added 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * _abs - computes the absolute value of an integer
  * @x: the integer to print
  *
- * Return: Always 0.
+ * Return: x.
  *
  */
 
@@ -13,17 +13,14 @@ int _abs(int x)
 	if (x < 0)
 	{
 		x = (x * -1);
-		_putchar(x);
 	}
 	else if (x > 0)
 	{
 		x = (x * 1);
-		_putchar(x);
 	}
 	else
 	{
 		x = (x * 0);
-		_putchar(x);
 	}
-	return (0);
+	return (x);
 }
