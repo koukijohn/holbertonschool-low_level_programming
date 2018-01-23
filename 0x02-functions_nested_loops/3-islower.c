0b02b62b9a4b42cc24a95cae26b03693d53f894b
KoukiJohn <koukijohn@hotmail.com>
2018-01-23 21:38:39 +0000

    added 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 
 /**
  * _islower - This checks for lowercase characters
- *
- * @c - character to check
+ * @c: character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise.
  *
@@ -15,8 +14,5 @@ int _islower(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
