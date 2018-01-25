10b0766bec65d95eee828bd5cdb97fdb91aff234
KoukiJohn <koukijohn@hotmail.com>
2018-01-25 23:49:30 +0000

    modified 4-print.c

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,10 @@ void print_most_numbers(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		if (!(x == 2) && !(x == 4))
-		_putchar(x);
+		if ((x != '2') && (x != '4'))
+		{
+			_putchar(x);
+		}
 	}
 	_putchar('\n');
 }
