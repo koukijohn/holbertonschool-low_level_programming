2e7223c63640f1dff35da27dc27689dcefc2b1df
KoukiJohn <koukijohn@hotmail.com>
2018-01-26 04:56:18 +0000

    added 8-print_square.c

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_square - This function prints a square, followed by a new line.
+ *
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
