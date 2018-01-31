3ecb3c75a97a8d987249ffa07029060c4a8aed1a
KoukiJohn <koukijohn@hotmail.com>
2018-01-31 03:20:50 +0000

    added 9-strcpy.c

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - This copies the string pointed by src.
+ * @dest: This is the source to copy the string to.
+ * @src: This is the string to copy.
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != 0; x++)
+		dest[x] = src[x];
+	dest[x] = 0;
+	return (dest);
+}
