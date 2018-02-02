b5d496d93887cfa21c21ba060a0b8bb3592ed8e0
KoukiJohn <koukijohn@hotmail.com>
2018-02-02 21:18:20 +0000

    added 2-strncpy.c

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int x;
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
-	{
-		for (; x < n; x++)
-		{
-			dest[x] = '\0';
-		}
-	}
+		dest[x] = src[x];
+	for ( ; x < n; x++)
+		dest[x] = '\0';
+
 	return (dest);
 }
