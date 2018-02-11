ffca93c1ac02858c6dda55cc607f00d5f186d225
KoukiJohn <koukijohn@hotmail.com>
2018-02-11 00:47:24 +0000

    added 0-whatsmyname.c

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program prints name, followed by new line.
+ * @argc: This is our argument counter.
+ * @argv: This is our argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+	return (0);
+}
