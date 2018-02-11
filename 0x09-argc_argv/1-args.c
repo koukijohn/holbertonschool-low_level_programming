5b3b8e53d63f8006a96f31a57356ed5c82c8a4ee
KoukiJohn <koukijohn@hotmail.com>
2018-02-11 00:54:06 +0000

    added 1-args.c

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program prints number of arguments passed into it.
+ * @argc: This is our argument counter.
+ * @argv: This is our argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void) argv;
+	return (0);
+}
