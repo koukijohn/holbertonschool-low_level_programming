9f5a7c9eb287aabcc21f6b450870ba24325835b8
KoukiJohn <koukijohn@hotmail.com>
2018-02-15 04:17:42 +0000

    added 1-strdup.c

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -15,16 +15,16 @@ char *_strdup(char *str)
 	int y;
 	char *GermGerm;
 
-	if (str == NULL)
+	if (str == NULL) /* This is given to us to check str memory */
 		return (NULL);
-	for (x = 0; str[x] != '\0'; x++)
+	for (x = 0; str[x] != '\0'; x++) /* To find str length */
 		;
-	GermGerm = malloc(sizeof(char) * x + 1);
-	if (GermGerm == NULL)
+	GermGerm = malloc(sizeof(char) * x + 1); /* add 1 to include null byte */
+	if (GermGerm == NULL) /* to check if germgerm has enought mem */
 		return (NULL);
-	for (y = 0; y <= x; y++)
+	for (y = 0; y <= x; y++) /* This makes the strings equal to each other*/
 	{
 		GermGerm[y] = str[y];
 	}
-	return (GermGerm);
+	return (GermGerm); /* Must return a char(our str) our copy */
 }
