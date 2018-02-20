f98e4a877a0b4e0fcdf0bc56a22c876744bb9025
KoukiJohn <koukijohn@hotmail.com>
2018-02-20 04:58:24 +0000

    added dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef Germ
+#define Germ
+
+/**
+ * struct dog - this will define a new type.
+ * @name: This is the dogs name.
+ * @age: This ios the dogs age.
+ * @owner: This is the dogs owner.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
