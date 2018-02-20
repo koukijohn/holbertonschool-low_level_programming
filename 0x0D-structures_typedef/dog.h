af170deed9176ddaa54f709c802c0b4c1ec40201
KoukiJohn <koukijohn@hotmail.com>
2018-02-20 18:24:43 +0000

    dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  */
 
-typedef struct dog /*added typedef for #3*/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t; /* added for #3 */
+} dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
