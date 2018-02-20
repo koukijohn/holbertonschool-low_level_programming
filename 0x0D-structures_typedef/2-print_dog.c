3f8c1d9327fb369ef50d52983855a7c743d3cc25
KoukiJohn <koukijohn@hotmail.com>
2018-02-20 18:28:35 +0000

    changed edge case

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,17 +11,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL) /*edge case that is given */
-		return;
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
+	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+		}
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
 }
