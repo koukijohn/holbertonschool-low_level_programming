6c06c4a572fc0709a59c89562a65bcdd3af3169a
KoukiJohn <koukijohn@hotmail.com>
2018-02-20 07:19:57 +0000

    added 2-printdog.c

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -15,8 +15,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n"); /* This is given*/
-        if (d->owner == NULL)
-                printf("Owner: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
 	else
 	{
 		printf("Name: %s\n", d->name);
