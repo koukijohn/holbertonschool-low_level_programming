4767571c5392b364621d0a3da9fc1f9e16d4bc27
KoukiJohn <koukijohn@hotmail.com>
2018-02-20 22:37:24 +0000

    added 5-free_dog.c

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - This function frees dogs.
+ * @d: This is the dog we are freeing
+ *
+ * Return: Void.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d!= NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
