f615faebcd75c56ab96022b66debd1eead83d9bd
KoukiJohn <koukijohn@hotmail.com>
2018-02-22 01:25:20 +0000

    added function_pointers.h

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef Johniscool
+#define Johniscool
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -8,3 +11,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
