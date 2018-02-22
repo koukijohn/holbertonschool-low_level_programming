c82256279285246fa9f605883312fcc739381df8
KoukiJohn <koukijohn@hotmail.com>
2018-02-22 03:11:20 +0000

    added 3-op_functions.c

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - This adds.
+ * @a: This is our first intger.
+ * @b: This is our second integer.
+ *
+ * Return: a+b.
+ */
+
+int op_add(int a, int b)
+	return (a + b);
+
+/**
+ * op_sub - This subtracts.
+ * @a: This is our first integer.
+ * @b: This is our second integer.
+ *
+ * Return: a-b.
+ */
+
+int op_sub(int a, int b)
+	return (a - b);
+/**
+ * op_mul - This multiplies.
+ * @a: This is our first integer.
+ * @b: This is our second integer.
+ *
+ * Return: a*b.
+ */
+
+
+int op_mul(int a, int b)
+	return (a * b);
+
+/**
+ * op_div - This divides.
+ * @a: This is our first integer.
+ * @b: This is our second integer.
+ *
+ * Return: a/b.
+ */
+
+int op_div(int a, int b)
+	return (a / b);
+
+/**
+ * op_mod - This remaindifies.
+ * @a: This is our first integer.
+ * @b: This is our second integer.
+ *
+ * Return: Remainder.
+ */
+
+int op_mod(int a, int b)
+	return (a % b);
