3a42d4c0ca12e465c1198714107fd70494a3ba67
KoukiJohn <koukijohn@hotmail.com>
2018-02-24 19:21:21 +0000

    added variadic_functions struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+/**
+ * struct args - struct arguments
+ * @args: These are the arguments being used like "f", "i", "s", "c"
+ * @f: This is a function pointer to va_list
+ */
+
+typedef struct args
+{
+	char *args; /*This points to "f", "i", "s", "c"*/
+	void (*f)(va_list);/*We name this function to pointer*/
+} Args;/*We name this struct hihi*/
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
