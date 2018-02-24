3998f8e329f682aee893eba588287d2231fff53b
KoukiJohn <koukijohn@hotmail.com>
2018-02-24 17:47:05 +0000

    added 2-print_strings.c

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - This is a function that prints strings
+ * @n: This is the number of strings passed to the function.
+ * @separator: This is the string in b/w our strings.
+ *
+ * Return: Void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list apple;
+
+	unsigned int x;
+	char *mango;
+
+	va_start(apple, n);
+
+	for (x = 0; x < n; x++)
+	{
+		mango = va_arg(apple, char *);
+		if (mango == NULL)
+			printf("(nil)\n");
+
+		printf("%s", mango);
+
+		if (separator == NULL)
+			return;
+		if (x < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(apple);
+}
