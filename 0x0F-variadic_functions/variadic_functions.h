2bf522e5b612c6ff52d03c267682a78a62be8826
KoukiJohn <koukijohn@hotmail.com>
2018-02-24 23:00:09 +0000

    modified variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdarg.h>
 
 /**
  * struct op - struct arguments
@@ -7,10 +8,20 @@
  * @f: This is a function pointer to va_list
  */
 
+typedef struct op
+{
+	char *args; /*This points to "f", "i", "s", "c"*/
+	void (*f)(va_list germo);
+} opx;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _printchar(va_list germo);
+void _printinteger(va_list germo);
+void _printfloat(va_list germo);
+void _printstring(va_list germo);
 
 #endif
