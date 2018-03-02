716625a20cf86b7f6cb932a03612ee7bbecbc361
KoukiJohn <koukijohn@hotmail.com>
2018-03-02 00:18:08 +0000

    added README.md lists.h and 1-list_len.c

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - This function will print all of the elements of a list_t list.
+ * @h: This is our list variable.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *str = h; /*h == head/declare head first with linked list*/
+	size_t numbofnodes = 0;/*return #ofnodes*/
+
+	if (str == NULL)
+	{
+		printf("[0] (nil)");
+		exit(0);
+	}
+	while (str != NULL)
+	{
+		numbofnodes++;
+		str = str->next;
+	}
+	return (numbofnodes);
+}
