2ab7695d26b31f072cceec042a2c82ab9ee93dc0
KoukiJohn <koukijohn@hotmail.com>
2018-03-07 02:09:32 +0000

    made temp null

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
-		*head = (*head)->next;
 	while (*head != NULL)
 	{
 		temp = *head;
+		temp = NULL;
 		*head = (*head)->next;
 		free(temp);
 	}
