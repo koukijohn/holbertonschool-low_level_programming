7e72b20d0d85a1b905369416855ea959e5269a5b
KoukiJohn <koukijohn@hotmail.com>
2018-03-07 17:36:40 +0000

    moved if head null inside loop

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -3,19 +3,21 @@
 #include "lists.h"
 
 /**
+ * *get_nodeint_at_index - Returns nth node of linked list.
+ * @head: This is our linked list.
+ * @index: This is index of node.
  *
- *
- *
+ * Return: listint_t (nth node).
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 
-	if (head == NULL)
-		return (NULL);
 	while (counter < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		counter++;
 	}
