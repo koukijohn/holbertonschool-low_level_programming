2a4dabfc07ad5b2d3f4b3354623de59ad79c81c9
KoukiJohn <koukijohn@hotmail.com>
2018-03-07 17:32:48 +0000

    reworked thought process

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -3,36 +3,21 @@
 #include "lists.h"
 
 /**
- * *get_nodeint_at_index - Returns nth node of linked list.
- * @head: This is our linked list.
- * @index: This is our index var to count.
  *
- * Return: nth node of linked list.
+ *
+ *
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *list1, *list2;
-	unsigned int x = 0;/*var to count index*/
-
-	list1 = head;
-	list2 = head;
+	unsigned int counter = 0;
 
 	if (head == NULL)
 		return (NULL);
-
-	while (x < index)
+	while (counter < index)
 	{
-		if (list1->next != NULL)
-			list1 = list1->next;
-		else
-			return (NULL);
-		x++;
+		head = head->next;
+		counter++;
 	}
-	while (list1->next != NULL)
-	{
-		list1 = list1->next;
-		list2 = list2->next;
-	}
-	return (list2);
+	return (head);
 }
