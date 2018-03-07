6cac4801326c563f36a3368d72f1f27e4e899f4f
KoukiJohn <koukijohn@hotmail.com>
2018-03-07 01:32:02 +0000

    added 5_listint2.c

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - This is a function that frees a listint_t list.
+ * @head: This is the list we are freeing.
+ *
+ * Return : Void.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
