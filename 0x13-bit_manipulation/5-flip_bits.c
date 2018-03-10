9409e714724b56bb56d272a5446fc3710ace3ab8
KoukiJohn <koukijohn@hotmail.com>
2018-03-10 07:17:21 +0000

    added diff while cond case

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -14,13 +14,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int numofbits = 0;
 	unsigned int counter = 0;/*This is for our loop*/
-	unsigned long int flipper = n ^ m;/*num yuou get after n^m*/
+	unsigned long int flipper = n ^ m;/*num you get after n^m*/
 
-	while (counter < 63)
+	while (counter < sizeof(unsigned int) * 8)
 	{
 		if ((flipper & 1) == 1)/*num compares and == 1*/
 			numofbits++; /*add 1 to numofbits flipped*/
-		flipper = flipper >> 1;/* This is to count the next bit*/
+		flipper = (flipper >> 1);/* This is to count the next bit*/
 		counter++;/*to count through every num in binary*/
 	}
 	return (numofbits);
