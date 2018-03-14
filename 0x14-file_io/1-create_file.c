15288a5835726c73f65d1615993cee5400656f95
KoukiJohn <koukijohn@hotmail.com>
2018-03-14 02:11:52 +0000

    moved where I start opening my file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,22 +17,31 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int writetester;
 	int length = 0;
 
-	while (text_content[length] != '\0')
-	{
-		length++;
-	}
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 /*0400/S_IRUSR read perm, 0200/S_IWUSR write perm*/
-	if (fd == -1)
-		return (-1);
-	write(fd, text_content, length);
+        if (fd == -1)
+                return (-1);
+        if (filename == NULL)
+                return (-1);
+	if (text_content == NULL)
+	{
+		text_content = "";
+		close(fd);
+		return (1);
+	}
+        while (text_content[length])
+        {
+                length++;
+        }
+	writetester = write(fd, text_content, length);
+	if (writetester == -1)
+	{
+		close(fd);
+		return(-1);
+	}
 	close(fd);
 	return (1); /*Success :)*/
 }
