b75406a33c6805c52f36032883615b193fae201a
KoukiJohn <koukijohn@hotmail.com>
2018-05-11 20:08:09 +0000

    added 0-hash

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -17,12 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	array = malloc(size * sizeof(hash_node_t));
 	if (array == NULL)
 	{
+		free(array);
 		return(NULL);
 	}
 
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 	{
+		free(new_hash_table);
 		return NULL;
 	}
 
