3572ef4a5e460679b4341a80bc435961c72705d3
KoukiJohn <koukijohn@hotmail.com>
2018-05-11 20:06:18 +0000

    fixed betty

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+/**
+ * hash_djb2 - This implements the djb2 algorithm.
+ * @str: This is our string.
+ *
+ * Return: hash
+ */
+
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
