c6c8c9a3c5506a1f2455e2752a47c436ff356cda
KoukiJohn <koukijohn@hotmail.com>
2018-05-12 01:32:41 +0000

    added 3-hash_table_set.c

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - This adds an element to the hash table.
+ * @ht: This is the hash table you want to add or update the key/value to
+ * @key: This is the key. Key can not be empty string.
+ * @value: Value associated with the key. Value can be empty string.
+ *
+ * Return: 1 if succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int location;
+	hash_node_t *new_element;
+	hash_node_t *traverser;
+
+	new_element = malloc(sizeof(hash_node_t *));
+	if (new_element == NULL)
+	{
+		return (0);
+	}
+	new_element->key = strdup(key);
+	new_element->value = strdup(value);
+	new_element->next = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	location = key_index((const unsigned char *)key, ht->size);
+
+	traverser = ht->array[location];
+
+	while (traverser != NULL)
+	{
+		if (strcmp(key, traverser->key) == 0)
+		{
+			free(traverser->value);
+			traverser->value = strdup(value);
+			return (1);
+		}
+		traverser = traverser->next;
+	}
+
+	ht->array[location] = new_element;
+
+	return (1);
+}
