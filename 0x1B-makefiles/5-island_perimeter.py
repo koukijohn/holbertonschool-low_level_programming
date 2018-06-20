b3366b8192e02d93c660992781d378c748231075
KoukiJohn <koukijohn@hotmail.com>
2018-06-20 17:03:06 +0000

    updated documentation

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """ This returns the perimeter of an island on a grid.
-"""
+Grid is a list of list of integers.
+- 0 represents a water zone.
+- 1 represents a land zone.
+- One cell is a square with a side length of 1.
+- Grid cells are connected horizontally/vertically (not diagonally).
+- Grid rectangular.
+- The width and height don't exceed 100.
 
+Grid is completely surrounded by water with only one island (or no island)
+Island has no lakes (no water within the island)
+"""
