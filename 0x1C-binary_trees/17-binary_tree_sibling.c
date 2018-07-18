c31b8e203c2ba44bb36b055d6da61d6d6497c873
koukijohn <koukijohn@github.com>
2018-07-18 18:06:30 +0000

    fixed 17-binary_tree_sibling.c

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -11,8 +11,12 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent;
 
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 	parent = node->parent;
-	if (node == NULL || parent == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
