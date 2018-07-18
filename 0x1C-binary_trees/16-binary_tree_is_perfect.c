7ada95ef02b46922083c820f2420d48899b1b3cf
koukijohn <koukijohn@github.com>
2018-07-18 17:41:40 +0000

    return 0 if failed

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -64,11 +64,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	if (tree->left != NULL && tree->right != NULL)
 	{
