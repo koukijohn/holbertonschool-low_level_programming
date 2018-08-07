4d43119d2a4d8f662459824980dcbe30f140afd0
koukijohn <koukijohn@github.com>
2018-08-07 21:05:31 +0000

    added 0-linear.c and search_algos.h

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - This is a function that searches for a value in an array
+ * of integers using the Linear search algorithm.
+ * @array: This is a pointer to the first element of the array to search in.
+ * @size: This is the number of elements in array.
+ * @value: This is the value to search for.
+ *
+ * Return: 1 for success. -1 for failure.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t counter = 0;
+
+	while (counter != size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", counter,
+		       array[counter]);
+		if (value == array[counter])
+		{
+			return (1);
+		}
+		counter++;
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __search_algos_h__
+#define __search_algos_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /** __search_algos_h__ **/
