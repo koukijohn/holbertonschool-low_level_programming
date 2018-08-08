c7b02fb97698a971f3f5d89a98a9fa8d4eef527b
koukijohn <koukijohn@github.com>
2018-08-08 16:51:05 +0000

    added 1-binary.c method without recursion

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,6 +1,29 @@
 #include "search_algos.h"
 
 /**
+ * printer - This is a function that will print Searching in array.
+ * @array: This is a pointer to the first element of the array to search in.
+ * @Left: This is the beginning of the array.
+ * @Right: This is the end of the array.
+ *
+ * Return: Void.
+ */
+
+
+void printer(int *array, size_t Left, size_t Right)
+{
+	size_t index = 0;
+
+	printf("Searching in array: ");
+	for (index = Left; index < Right; index++)
+	{
+		printf("%d, ", array[index]);
+	}
+	printf("%d\n", array[index]);
+}
+
+
+/**
  * binary_search - This is a function that searches for a value in an array
  * of integers using the Binary search algorithm.
  * @array: This is a pointer to the first element of the array to search in.
@@ -14,23 +37,15 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t Left = 0;
 	size_t Right = size - 1;
-	size_t midpoint;
-	size_t index = 0;
+	size_t midpoint = 0;
 
 	while (Left <= Right)
 	{
 		midpoint = ((Left + Right) / 2);
 
 		/* This will print our searching in array */
-		printf("Searching in array: ");
-		while (index < Right)
-		{
-			printf("%d, ", array[index]);
-			index++;
-		}
-		printf("%d\n", array[index]);
+		printer(array, Left, Right);
 		/* This will print our searching in array */
-
 
 		if (array[midpoint] < value)
 		{
@@ -44,7 +59,6 @@ int binary_search(int *array, size_t size, int value)
 		{
 			return (midpoint);
 		}
-		Left++;
 	}
 	return (-1);
 }
