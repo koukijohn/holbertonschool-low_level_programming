7e83a0b0bd687a123a1b6e326e49307566bc11c7
koukijohn <koukijohn@github.com>
2018-08-29 20:32:06 +0000

    fixed size to n - 1

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -5,18 +5,20 @@
  * @a: An array of integers.
  * @n: The number of elements to swap.
  *
+ * Return: Void.
  */
 
 void reverse_array(int *a, int n)
 {
 	int temp;
 	int start;
+	n = n - 1;
 
-	for (start = 0; start < n; start++)
+	for (start = 0; start < n; n--)
 	{
-		temp = a[n - 1];
-		a[n] = a[start];
-		a[start] = temp;
-		n--;
+		temp = a[start];
+		a[start] = a[n];
+		a[n] = temp;
+		start++;
 	}
 }
